1cdb7ac1f741e66cecbb8a4d829813e0f5c0c915
Andres Jacob <42328105+ajrodriguezo@users.noreply.github.com>
2023-11-01 11:32:10 -0500

    #docstring

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+# Docstring
 
+def palindromo (senctence: str) -> bool:
+    """Esta funcion permite conocer si un string
+    es un palindromo , o no, es decir, que se puede 
+    leer igual de  izquierda a derecha que de derecha a izquierda
+
+    Args:
+        sentence: string
+
+    Returns:
+        bool
+    """
+
+    senctence = senctence.lower().replace(' ','')
+    return senctence == senctence[::-1]
