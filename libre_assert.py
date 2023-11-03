c2722f95a8c5f3780ec917298cce0afd82a3185a
BIOMECJacob <jacob.rodriguez@biomec.com.co>
2023-11-03 15:06:34 -0500

    yes

--- libre_assert.py
+++ libre_assert.py
@@ -4,13 +4,13 @@ if __name__ == '__main__':
     
     try:
         assert 5==10 , 'lo sentimos 5 no es igual a 10'
-        '''
+        """
         if not 5 == 10:
             raise AssertionError('lo sentimos 5 no es igual a 10')
             
         en python es mejor lanzar errores que condicionar
         por eso es mejor la sentencia assert
-        '''
+        """
 
         print('El programa continua con su ejecucion ')
     
