a1621840aca37788a6371c0db339b413603fd37f
BIOMECJacob <jacob.rodriguez@biomec.com.co>
2023-11-07 15:57:59 -0500

    fin assert parte 2

--- funcionsuma.py
+++ funcionsuma.py
@@ -8,7 +8,18 @@ def suma_numeros_positivo(n1: int, n2: int)-> int:
     Returns:
         int:
     """
-    return n1+n2
+    #if n1<0 or n2<0 :
+    #    return None
+    
+    try:    
+        assert n1 > 0 and n2 > 0, 'Lo sentimos, solo es posible sumar numeros enteros'
+        
+        return n1+n2
+    
+    except AssertionError as error:
+        print(error)
+        
+        
 
 if __name__ == '__main__':
     resultado = suma_numeros_positivo(10,20)
