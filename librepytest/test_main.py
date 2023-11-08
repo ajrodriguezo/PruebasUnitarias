95619281cf8ab37f444b808569d34492af278236
BIOMECJacob <jacob.rodriguez@biomec.com.co>
2023-11-08 16:36:17 -0500

    ejecutar pruebas que quiero

--- librepytest/test_main.py
+++ librepytest/test_main.py
@@ -1,5 +1,17 @@
 import pytest
 
-def test_example():
-    assert 10 == 1, 'Lo sentimos, prueba no ha pasado'
+#def test_example():
+#    assert 10 == 1, 'Lo sentimos, prueba no ha pasado'
 
+class TestExample():
+    
+    def test_suma_dos_numeros(self):
+        assert 10 + 10 == 20, 'Lo sentimos, la suma no es correcta.'
+        
+    def test_resta_dos_numeros(self):
+        assert 10 - 10 == 0, 'Lo sentimos, la resta no es correcta.'
+
+class TestExample2():
+
+    def test_multipplicacion_dos_numeros(self):
+        assert 2 * 10 == 20
