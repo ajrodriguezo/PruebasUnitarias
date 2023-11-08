ad9f7bd69eed6a9aa7b55cb4398ee6b547364d2f
BIOMECJacob <jacob.rodriguez@biomec.com.co>
2023-11-08 17:01:58 -0500

    estructura del prit¿yecto

--- librepytest/test_main.py
+++ librepytest/test_main.py
@@ -5,11 +5,29 @@ import pytest
 
 class TestExample():
     
+    @classmethod
+    def setup_class(cls):
+        print('>>> setup_class se ejecuta antes de todas las pruebas')
+    
+    @classmethod
+    def teardown_class(cls):
+        print('>>> teardown_class se ejecuta despues de todas las pruebas')
+
+    
+    def setup_method(self):
+        #print('>>> El metodo setup se ejecuta antes de cada prueba')
+        self.numero_uno = 10
+        self.numero_dos = 20
+        
+    
+    def teardown_method(self):
+        print('>>> El metodo teardown se ejecuta despues de cada prueba')
+    
     def test_suma_dos_numeros(self):
-        assert 10 + 10 == 20, 'Lo sentimos, la suma no es correcta.'
+        assert self.numero_uno + self.numero_dos == 30, 'Lo sentimos, la suma no es correcta.'
         
     def test_resta_dos_numeros(self):
-        assert 10 - 10 == 0, 'Lo sentimos, la resta no es correcta.'
+        assert self.numero_uno - self.numero_dos == -10, 'Lo sentimos, la resta no es correcta.'
 
 class TestExample2():
 
