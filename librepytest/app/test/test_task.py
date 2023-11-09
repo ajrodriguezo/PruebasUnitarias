253b38ce0a3bbcc011b32bc9ff58ff3ef592bf07
BIOMECJacob <jacob.rodriguez@biomec.com.co>
2023-11-09 08:37:06 -0500

    primeros pasos

--- librepytest/app/test/test_task.py
+++ librepytest/app/test/test_task.py
@@ -1,6 +1,25 @@
 import pytest
 
+from datetime import datetime
+from datetime import timedelta
+
+from app.task import Task 
+
 class TestTask():
     
     def test_task(self):
-        assert True
+        pass
+    
+    def test_new_task(self):
+        due_date = datetime.now()
+        
+        task = Task('Title', 'Description', 'eduardo', due_date)
+        
+        assert task.title == 'Title'
+        assert task.description == 'Description'
+        assert task.assigned_to == 'eduardo'
+        assert task.due_date == due_date
+        
+    def test_due_date_error(self):
+        due_date = datetime.now() - timedelta(days=1)
+        Task('Title', 'Description', 'eduardo', due_date)
