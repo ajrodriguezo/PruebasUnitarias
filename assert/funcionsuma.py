f118f1a0578310b84ab35e480dbdb23c08eb886a
BIOMECJacob <jacob.rodriguez@biomec.com.co>
2023-11-14 15:24:31 -0500

    modulo logging

--- assert/funcionsuma.py
+++ assert/funcionsuma.py
@@ -1,3 +1,20 @@
+import logging
+
+# TIPOS || VALOR || FUNCION
+#DEBUNG = 10 = debug
+#INFO = 20 = info
+#WARNING 30 = warning
+# ERROR = 40 = error
+# CRITICAL = 50 = critical
+
+logging.basicConfig(level=logging.DEBUG,
+                    format="%(threadName)s - %(levelname)s - %(asctime)s - Message:%(message)s",
+                    datefmt="%Y/%m/%d %H:%M:%S",
+                    filename="cursoCodigoFacilito.log",
+                    filemode="a"
+                    )
+# por default siempre se ven los mayores o iguales a 30
+
 def suma_numeros_positivo(n1: int, n2: int)-> int:
     """Permite sumar 2 numero enteros positivos
 
@@ -11,6 +28,8 @@ def suma_numeros_positivo(n1: int, n2: int)-> int:
     #if n1<0 or n2<0 :
     #    return None
     
+    logging.debug('antes de sumar')
+    
     try:    
         assert n1 > 0 and n2 > 0, 'Lo sentimos, solo es posible sumar numeros enteros'
         
@@ -18,10 +37,14 @@ def suma_numeros_positivo(n1: int, n2: int)-> int:
     
     except AssertionError as error:
         print(error)
+
+    logging.debug('despues de sumar')
         
         
 
 if __name__ == '__main__':
+    logging.debug('antes del llamdo')
+    
     resultado = suma_numeros_positivo(10,20)
-    print(resultado)
+    logging.info(resultado)
     
